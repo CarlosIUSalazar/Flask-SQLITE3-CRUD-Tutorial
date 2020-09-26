87618ba547c53dfa6bd37a29549cc9e7910e786d
CarlosIUSalazar <carlosiusalazar@gmail.com>
2020-09-26 12:19:44 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, g
+import sqlite3
+
+app = Flask(__name__)
+
+def connect_db():
+    sql = sqlite3.connect('/')
+    sql.row_factory = sqlite3.Row
+    return sql
+
+
+def get_db():
+    #Check if DB is there
+    if not hasattr(g, 'sqlite3'):
+        g.sqlite3_db = connect_db()
+    return g.sqlite3_db
+
+#close the connection to the database automatically
+@app.teardown_appcontext
+def close_db(error):
+    #if global object has a sqlite database then close it. If u leave it open noone can access it and gets lost in memory causing leaks.
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite3_db.close()
+
+@app.route('/')
+def index():
+    return '<h1>Hello, World!</h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
